4e5ac1a9aa6ec861606aaca858ee05a27c0eb75f
Janovits Hunor <janovitshuni@gmail.com>
2023-11-26 15:02:39 +0100

    elearning rendszer módosítása

---
+++ lms/admin/footer.php
@@ -0,0 +1,5 @@
+<div class="pull-right">
+		<footer>
+           <p><strong>GitBarmok Elearning Kezelői Felülete @ 2023</strong></p>
+        <footer>
+</div>
---
+++ lms/developers.php
@@ -0,0 +1,58 @@
+<?php include('header_dashboard.php'); ?>
+    <body id="class_div">
+		<?php include('navbar_about.php'); ?>
+        <div class="container-fluid">
+            <div class="row-fluid">
+                <div class="span12" id="content">
+                     <div class="row-fluid">
+                        <!-- block -->
+                        <div class="block">
+								<div class="navbar navbar-inner block-header">
+									<div id="" class="muted pull-right"><a href="index.php"><i class="icon-arrow-left"></i> Visszalépés</a></div>
+								</div>
+                            <div class="block-content collapse in">
+							<h3>Developers</h3>
+							<hr>
+                                <div class="span3">
+										<center>
+										<img id="developers" src="admin/images/jkev.jpg" class="img-circle">
+										<hr>
+										<p>Név: Berencsei Attila</p>
+										<p>Beosztás: Fejlesztő</p>
+										</center>
+								</div>
+                                <div class="span3">
+								<center>
+								<img id="developers" src="admin/images/jelyn.jpg" class="img-circle">
+								<hr>
+										<p>Név: Hadadi Benjamin</p>
+										<p>Beosztás: Fejlesztő</p>
+								</center>
+								</div>
+                                <div class="span3">
+								<center>
+								<img id="developers" src="admin/images/jorge.jpg" class="img-circle">
+								<hr>
+										<p>Name: Janovits Hunor</p>
+										<p>Beosztás: Fejlesztő / magyarosító</p>
+								</center>
+								</div>
+                                <div class="span3">
+								<center>
+								<img id="developers" src="admin/images/mich.jpg" class="img-circle">
+								<hr>
+										<p>Név: Tamás Etele</p>
+										<p>Beosztás: Fejlesztő</p>
+								</center>
+								</div>
+                            </div>
+                        </div>
+                        <!-- /block -->
+                    </div>
+                </div>
+            </div>
+		<?php include('footer.php'); ?>
+        </div>
+		<?php include('script.php'); ?>
+    </body>
+</html>
